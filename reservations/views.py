881b070f0fc3bcfce8883fd35c3e51b6eae1660c
jlruedam <jlruedam@gmail.com>
2025-09-02 11:37:01 -0500

    Add footer and header CSS files, update styles and images, and refactor HTML structure

--- reservations/views.py
+++ reservations/views.py
@@ -4,6 +4,5 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def home(request):
-    
     ctx = {}
     return render(request, 'home/index.html', ctx)
